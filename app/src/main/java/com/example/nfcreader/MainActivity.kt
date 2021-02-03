1623a6e43a176c043c41fd404b668396a04f2324
kanido386 <kanido386@gmail.com>
2021-02-03 16:01:34 +0800

    Enable foreground dispatch

--- app/src/main/java/com/example/nfcreader/MainActivity.kt
+++ app/src/main/java/com/example/nfcreader/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.nfcreader
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.ACTION_NFC_SETTINGS
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private var nfcAdapter: NfcAdapter? = null
+    private var pendingIntent: PendingIntent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +19,36 @@ class MainActivity : AppCompatActivity() {
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
         if (nfcAdapter == null) {
-            Toast.makeText(this, "沒開 NFC 哦！", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "No NFC", Toast.LENGTH_SHORT).show()
             finish()
             return
         }
+
+        val intent = Intent(this, this.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val nfcAdapterRefCopy = nfcAdapter
+        if (nfcAdapterRefCopy != null) {
+            if (!nfcAdapterRefCopy.isEnabled()) {
+                showNFCSettings()
+            }
+            nfcAdapterRefCopy.enableForegroundDispatch(this, pendingIntent, null, null)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+    }
+
+    private fun showNFCSettings() {
+        Toast.makeText(this, "You need to enable NFC!", Toast.LENGTH_SHORT).show()
+        val intent = Intent(ACTION_NFC_SETTINGS)
+        startActivity(intent)
     }
 }
