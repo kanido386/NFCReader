9b845e3292425449128fa95e863016b3f3326a9f
kanido386 <kanido386@gmail.com>
2021-02-03 10:51:20 +0800

    Initialize NFC adapter

--- app/src/main/java/com/example/nfcreader/MainActivity.kt
+++ app/src/main/java/com/example/nfcreader/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.nfcreader
 
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var nfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "沒開 NFC 哦！", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
     }
 }
